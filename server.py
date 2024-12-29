cd0f4fe954771c20b425b7365425c5a196742b53
smallsinger629 <1806889377@qq.com>
2024-12-29 21:01:12 +0800

    Update server.py

--- server.py
+++ server.py
@@ -127,7 +127,7 @@ def set_normal():
         d.dset('status', status)
         # 如果电量值存在，修改应用名称，显示电量
         if battery:
-            app_name += f"（电量：{battery}）"
+            app_name += f"（电量:{battery}%）"
         d.dset('app_name', app_name)
 
         # 获取当前时间并转换为 UTC+8 时区
